311269700407fa703d947af746ab809d5beb15e0
MW <michael60457@gmail.com>
2024-04-19 21:57:57 +0800

    demo_v2

--- demo/demo.py
+++ demo/demo.py
@@ -11,24 +11,6 @@ kmeans_model = None
 qsize = 30
 avg_thres = 0
 
-def dataset(time, thres, add_val):
-    path = 'dataset/dataset1.txt'
-    with open(path) as f:
-        for line in f.readlines():
-            if line.strip() == '-----------------------------':
-                add_val += 10
-                continue
-            parts = line.split()
-            if parts[0] == 'stop' and add_val < 30:
-                thres.append(float(parts[1]))
-            elif parts[0] == 'move' and parts[1] == 'forward' and add_val < 30:
-                thres.append(float(parts[2]))
-            else:
-                if add_val < 30:
-                    if float(parts[0]) > 10:
-                        parts[0] = str(float(parts[0]) - 10)
-                    time.append(float(parts[0]) + add_val)
-
 def kmeans_f2(thres, time):
     global kmeans_model
     features = thres.reshape(-1, 1)
@@ -54,27 +36,8 @@ def kmeans_f2(thres, time):
     avg_thres = thres_sum / cnt
     return avg_thres   
     
-    # plt.scatter(time[:len(labels)], thres[:len(labels)], c=labels, cmap='viridis', edgecolor='k', s=50, alpha=0.7)
-    # plt.scatter(time[:len(labels)], [avg_thres]*len(labels), color='red', s=5, marker='o', label='Average Threshold') 
-    # plt.xlabel('Time')
-    # plt.ylabel('Threshold')
-    # plt.title('K-Means Clustering of Alpha Band Power')
-    # plt.colorbar(label='Cluster')
-    # plt.show()
-
-
-
 def main():
-
-    time = []
-    thres = []
-    add_val = 0
-    dataset(time, thres, add_val)
-    # time = np.array(time)
-    # thres = np.array(thres)
-    
-    # kmeans_f2(thres, time)
-    
+  
     # ----------------- Test -----------------
     # test = thres[70:85].reshape(1, -1)
     # if kmeans_model is not None:
@@ -89,8 +52,9 @@ def main():
     streams = resolve_stream('name', 'OpenViBE Stream1')
     inlet = StreamInlet(streams[0])
     start = monotonic()
-    thres = []
-    time = []
+    Thres = []
+    Time = []
+    collect_data = False
     while True:
         sample, timestamp = inlet.pull_chunk()
         if timestamp:
@@ -100,37 +64,44 @@ def main():
             q.put(sample)
 
             ratio = sum(list(q.queue)) / q.qsize()
-            thres.append(ratio)
             end = monotonic()
-            time.append(end - start)
-            if end - start > 30:
-                q.queue.clear()
-                break
-   
-    thres = np.array(thres)
-    time = np.array(time)
-    avg_thres = kmeans_f2(thres, time)
-    print(avg_thres)
+            if not collect_data:
+                Thres.append(ratio)
+                Time.append(end - start)
+            if end - start > 30 and not collect_data:
+                avg_thres = kmeans_f2(np.array(Thres), ap.array(Time))
+                print("Average Threshold:", avg_thres)
+                collect_data = True
 
-    while True:
-        sample, timestamp = inlet.pull_chunk()
-        if timestamp:
-            sample = sample[0][0]
-            # print(sample)  # find fish
-            while q.qsize() >= qsize:
-                _ = q.get()
-            q.put(sample)
+            if collect_data:
+                if ratio > avg_thres and q.qsize() == qsize:
+                    print("move forward", ratio)
+                else:
+                    print("stop ", ratio)
 
-            ratio = sum(list(q.queue)) / q.qsize()
-            
-            if ratio > avg_thres and q.qsize() == qsize:
-                print("move forward", ratio)
-            else:
-                print("stop ", ratio)
         time.sleep(0.2)
+   
+    # thres = np.array(thres)
+    # time = np.array(time)
+    # avg_thres = kmeans_f2(thres, time)
+    # print(avg_thres)
 
+    # while True:
+    #     sample, timestamp = inlet.pull_chunk()
+    #     if timestamp:
+    #         sample = sample[0][0]
+    #         # print(sample)  # find fish
+    #         while q.qsize() >= qsize:
+    #             _ = q.get()
+    #         q.put(sample)
 
-
+    #         ratio = sum(list(q.queue)) / q.qsize()
+            
+    #         if ratio > avg_thres and q.qsize() == qsize:
+    #             print("move forward", ratio)
+    #         else:
+    #             print("stop ", ratio)
+    #     time.sleep(0.2)
 
 if __name__ == '__main__':
     try:
